4792ec15812462a871b6fab5fa63a217b41efd46
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-04-13 20:31:36 -0300

    fix: consertando as funções

--- src/listaDeExerciciosInterface/Arma.java
+++ src/listaDeExerciciosInterface/Arma.java
@@ -3,5 +3,7 @@ package listaDeExerciciosInterface;
 public interface Arma {
 
 	public void atirar(int quantidadeTiro);
+	
 	public void recarregar(int tempoRecarga);
+	
 }
--- src/listaDeExerciciosInterface/Pistola.java
+++ src/listaDeExerciciosInterface/Pistola.java
@@ -4,12 +4,15 @@ public class Pistola implements Arma {
 
 	@Override
 	public void atirar(int quantidadeTiro) {
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void recarregar(int tempoRecarga) {
+		// TODO Auto-generated method stub
 		
 	}
+
 
 }
